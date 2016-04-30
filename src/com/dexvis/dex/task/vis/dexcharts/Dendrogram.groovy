4ec7b00d761c65c19bf583aa1516d53895a85309
Patrick Martin <patrick145@msn.com>
2016-04-30 00:42:43 -0400

    Took out connection.length argument so fit-text could be default.

--- src/com/dexvis/dex/task/vis/dexcharts/Dendrogram.groovy
+++ src/com/dexvis/dex/task/vis/dexcharts/Dendrogram.groovy
@@ -13,14 +13,15 @@ class Dendrogram extends WebTask {
     "visualization/dex_charts/Dendrogram.html",
     'web/dexcharts/Dendrogram.gtmpl')
 
+ 
     setProperty('Root Node', 'Name', 'root.name', '')
     setProperty('Root Node', 'Category', 'root.category', '')
-    
+
     setProperty('Dimensions', 'Height', 'height', 600)
     setProperty('Dimensions', 'Width', 'width', 800)
     setProperty('Dimensions', 'SVG Transform', 'transform', '')
     
-    setProperty('Links', 'Length', 'connection.length', 180)
+    setProperty('Links', 'Length', 'connection.length', '180')
     
     addProperties(TaskPropertyFactory.createCircleProperties('Expanded Node', 'node.expanded.circle'))
     addProperties(TaskPropertyFactory.createCircleProperties('Collapsed Node', 'node.collapsed.circle'))
@@ -28,7 +29,7 @@ class Dendrogram extends WebTask {
     addProperties(TaskPropertyFactory.createTextProperties('Expanded Node Label', 'node.expanded.label'))
     addProperties(TaskPropertyFactory.createTextProperties('Collapsed Node Label', 'node.collapsed.label'))
     addProperties(TaskPropertyFactory.createLinkProperties('Links', 'link'))
-    
+   
     setSaveDynamic(true)
   }
 }
