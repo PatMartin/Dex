4fb72dedcbe66e2677a7c661433cf63ced9c50a9
Patrick Martin <patrick145@msn.com>
2017-04-27 10:30:09 -0400

    Deprecating scripts

--- scripts/jython/klingon3d.py
+++
@@ -1,8 +0,0 @@
-# Canvas3D example. Show 3D object using OFF format
-
-from jhpro.engine3d import *
-
-# all OFF files are located in /data directory
-view=HEngine3D("data/off/klingon.off")
-view.setBounds(10, 10, 800, 800)
-view.setVisible(1)
--- scripts/jython/random_sales.py
+++
@@ -1,8 +0,0 @@
-from java.util import Random
-
-rand = Random()
-
-header = [ 'MONTH', 'SALES', 'OVERHEAD', 'REVENUE', 'PROFIT' ]
-data = [ ]
-for month in ['JAN','FEB','MAR','APR','MAY','JUN','JUL','AUG','SEP','OCT','NOV','DEC']:
-  data.append([ month, rand.nextInt(1000), rand.nextInt(1000), rand.nextInt(1000), rand.nextInt(1000) ])
