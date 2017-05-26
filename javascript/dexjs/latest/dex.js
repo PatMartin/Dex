8ae9353133ea18104ef9af418f57c6022b100f36
Patrick Martin <patrick145@msn.com>
2017-05-26 16:01:44 -0400

    Refresh

--- javascript/dexjs/latest/dex.js
+++ javascript/dexjs/latest/dex.js
@@ -1608,6 +1608,7 @@ var chord = function (userConfig) {
         dex.config.gui.link({name: "Nodes on Mouseover"}, "nodes.mouseover"),
       ]
     };
+
     var guiDef = dex.config.expandAndOverlay(config, defaults);
     dex.config.gui.sync(chart, guiDef);
     return guiDef;
@@ -2257,7 +2258,7 @@ var dendrogram = function Dendrogram(userConfig) {
 
   chart = new dex.component(userConfig, defaults);
 
-  chart.getGuiDefinition = function getGuiDefinition(userConfig) {
+  chart.getGuiDefinition = function getGuiDefinition(config) {
     var defaults = {
       "type": "group",
       "name": "Dendrogram Settings",
@@ -2292,7 +2293,10 @@ var dendrogram = function Dendrogram(userConfig) {
         dex.config.gui.link({}, "link")
       ]
     };
-    return dex.config.expandAndOverlay(userConfig, defaults);
+
+    var guiDef = dex.config.expandAndOverlay(config, defaults);
+    dex.config.gui.sync(chart, guiDef);
+    return guiDef;
   };
 
   chart.render = function render() {
@@ -8022,123 +8026,157 @@ elegans.ScatterPlot = require("./ScatterPlot");
 module.exports = elegans;
 },{"./ScatterPlot":39}],41:[function(require,module,exports){
 var gridstermultiples = function (userConfig) {
-    var chart;
-    var gridster;
-    var cells = [];
+  var chart;
+  var gridster;
+  var cells = [];
 
-    var defaults = {
-        'parent': '#GridsterMultiplesParent',
-        'id': 'GridsterMultiplesId',
-        'class': 'GridsterMultiplesClass',
-        'resizable': true,
-        'frames': {},
-        'width': "100%",
-        'height': "100%",
-        'cell' : {
-          'width' : 6,
-          'height' : 6
+  var defaults = {
+    'parent': '#GridsterMultiplesParent',
+    'id': 'GridsterMultiplesId',
+    'class': 'GridsterMultiplesClass',
+    'resizable': true,
+    'frames': {},
+    'width': "100%",
+    'height': "100%",
+    'cell': {
+      'width': 6,
+      'height': 6
+    },
+    'baseChart': dex.charts.d3.Dendrogram(),
+    'gridsterConfig': {
+      widget_base_dimensions: [50, 50],
+      widget_margins: [1, 1],
+      //helper: 'clone',
+      animate: false,
+      draggable: {
+        enabled: true,
+        start: function (e, ui) {
+          dex.console.log("DRAG-START", e, ui);
         },
-        'baseChart': dex.charts.d3.Dendrogram(),
-        'gridsterConfig': {
-            widget_base_dimensions: [50, 50],
-            widget_margins: [1, 1],
-            //helper: 'clone',
-            animate: false,
-            draggable: {
-                enabled: true,
-                start: function (e, ui) {
-                    dex.console.log("DRAG-START", e, ui);
-                },
-                handle: '.drag-handle'
-            },
-            resize: {
-                enabled: true,
-                min_size: [1, 1],
-                stop: function (event, ui, $widget) {
-                    dex.console.log("Event", event, ui, $widget);
-                    cells.forEach(function (cell, i) {
-                        cell.render();
-                    })
-                }
-            }
+        handle: '.drag-handle'
+      },
+      resize: {
+        enabled: true,
+        min_size: [1, 1],
+        stop: function (event, ui, $widget) {
+          dex.console.log("Event", event, ui, $widget);
+          cells.forEach(function (cell, i) {
+            cell.render();
+          })
         }
+      }
+    }
+  };
+
+  chart = new dex.component(userConfig, defaults);
+
+  chart.getGuiDefinition = function getGuiDefinition(config) {
+    var defaults = {
+      "type": "group",
+      "name": "Multiples Configuration",
+      "contents": [
+        dex.config.gui.dimensions(),
+        dex.config.gui.general(),
+        {
+          "type": "group",
+          "name": "Miscellaneous",
+          "contents": [
+            {
+              "name": "Cell Width",
+              "description": "Cell Width",
+              "type": "int",
+              "minValue": 1,
+              "maxValue": 20,
+              "initialValue": 6,
+              "target": "cell.width"
+            },
+            {
+              "name": "Cell Height",
+              "description": "Cell Height",
+              "type": "int",
+              "minValue": 1,
+              "maxValue": 20,
+              "initialValue": 6,
+              "target": "cell.height"
+            }
+          ]
+        }
+      ]
     };
 
-    chart = new dex.component(userConfig, defaults);
+    var guiDef = dex.config.expandAndOverlay(config, defaults);
+    dex.config.gui.sync(chart, guiDef);
+    return guiDef;
+  };
 
-    chart.render = function render() {
-        var config = chart.config;
-        var frames = config.frames;
+  chart.render = function render() {
+    var config = chart.config;
+    var frames = config.frames;
 
-        d3.selectAll(config.parent).selectAll("*").remove();
+    if (config.charts) {
+      // Unregisters any window resize handlers.
+      config.charts.forEach(function(oldChart) { oldChart.deleteChart(); })
+    }
+    d3.selectAll(config.parent).selectAll("*").remove();
 
-        dex.console.log("FRAMES", frames);
+    //dex.console.log("FRAMES", frames);
 
-        var numFrames = frames.frames.length;
+    var numFrames = frames.frames.length;
 
-        var gridsterContainer = d3.select(config.parent)
-            .append("div")
-            .attr("id", config["id"])
-            .attr("class", "gridster")
-            .attr("width", config.width)
-            .attr("height", config.height)
-            .append("ul");
+    var gridsterContainer = d3.select(config.parent)
+      .append("div")
+      .attr("id", config["id"])
+      .attr("class", "gridster")
+      .attr("width", config.width)
+      .attr("height", config.height)
+      .append("ul");
 
-        dex.console.log("Invoking gridster constructor");
+    //dex.console.log("Invoking gridster constructor");
 
-        gridster = $(".gridster ul")
-            .gridster(config.gridsterConfig)
-            .data('gridster');
+    gridster = $(".gridster ul")
+      .gridster(config.gridsterConfig)
+      .data('gridster');
 
-        cells = [];
-        frames.frames.forEach(function (frame, i) {
-            /*            d3.select(".gridster ul")
-             .append("li")
-             .attr("data-row", Math.floor(i / config.numColumns + 1))
-             .attr("data-col", (i % config.numColumns + 1))
-             .attr("data-sizex", "1")
-             .attr("data-sizey", "1")
-             .append("div")
-             .attr("id", config.id + i)
-             .attr("class", config["class"] + "Cell");
-             */
-            var widget = "<li><table id='widget-table' style='word-break:break-all;'>" +
-                "<tr id='widget-header-row'><td id='widget-header' class='drag-handle'>" +
-                frames.frameIndices[i] +
-                "</td></tr>" +
-                "<tr><td id='widget-content' colspan='2'><div id='" + (config.id + i) +
-                "' height='100%' width='100%'></div></td></tr></table></li>"
+    cells = [];
+    frames.frames.forEach(function (frame, i) {
+      var widget = "<li><table id='widget-table' style='word-break:break-all;'>" +
+        "<tr id='widget-header-row'><td id='widget-header' class='drag-handle'>" +
+        frames.frameIndices[i] +
+        "</td></tr>" +
+        "<tr><td id='widget-content' colspan='2'><div id='" + (config.id + i) +
+        "' height='100%' width='100%'></div></td></tr></table></li>"
 
-            gridster.add_widget(widget, config.cell.width, config.cell.height);
+      gridster.add_widget(widget, config.cell.width, config.cell.height);
 
-            //         var cellChart = config.baseChart
-            //           .clone()
-            //         .attr("parent", "#" + config.id + i)
-            //       .attr("csv", frame);
-            var cellChart = config.baseChart.clone({
-                "parent": "#" + config.id + i,
-                "csv": frame
-            });
-            cellChart.render();
-            cells.push(cellChart);
-        });
-
-        return chart;
-    };
-
-    chart.update = function () {
-        cells.forEach(function (cell) {
-            cell.update();
-        })
-    };
-
-    $(document).ready(function () {
-        var config = chart.config;
-        // Make the entire chart draggable.
-        //$(chart.config.parent).draggable();
+      //         var cellChart = config.baseChart
+      //           .clone()
+      //         .attr("parent", "#" + config.id + i)
+      //       .attr("csv", frame);
+      var cellChart = config.baseChart.clone({
+        "parent": "#" + config.id + i,
+        "csv": frame
+      });
+      cellChart.render();
+      cells.push(cellChart);
     });
 
+    chart.config.charts = cells;
     return chart;
+  };
+
+  chart.update = function () {
+    cells.forEach(function (cell) {
+      cell.update();
+    })
+  };
+
+  $(document).ready(function () {
+    var config = chart.config;
+    // Make the entire chart draggable.
+    //$(chart.config.parent).draggable();
+  });
+
+  return chart;
 };
 
 module.exports = gridstermultiples;
