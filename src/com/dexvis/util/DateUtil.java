89fb6a90cfed9660e19ad73cdab8296ebf570286
PatMartin <patrick145@msn.com>
2018-03-13 11:21:19 -0400

    Changed date detection to be extensible and to impose regex to find the appropriate date format.

--- src/com/dexvis/util/DateUtil.java
+++ src/com/dexvis/util/DateUtil.java
@@ -7,36 +7,157 @@ import java.util.Date;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.apache.commons.lang3.StringUtils;
 
 public class DateUtil
 {
-  private static SimpleDateFormat df[] = {
-      new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'"),
-      new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssZ"),
-      new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'"),
-      new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSZ"),
-      new SimpleDateFormat("yyyy-MM-dd'T'hh:mm:ss"),
-      new SimpleDateFormat("yyyy-MM-dd HH:mm:ss"),
-      new SimpleDateFormat("MM/dd/yyyy'T'HH:mm:ss.SSS'Z'"),
-      new SimpleDateFormat("MM/dd/yyyy'T'HH:mm:ss.SSSZ"),
-      new SimpleDateFormat("MM/dd/yyyy'T'HH:mm:ss.SSS"),
-      new SimpleDateFormat("MM/dd/yyyy'T'HH:mm:ssZ"),
-      new SimpleDateFormat("MM/dd/yyyy'T'HH:mm:ss"),
-      new SimpleDateFormat("MM/dd/yyyy HH:mm:ss"),
-      new SimpleDateFormat("MM/dd/yyyy HH:mm"),
-      new SimpleDateFormat("MM/dd/yyyy"),
-      new SimpleDateFormat("yyyy:MM:dd HH:mm:ss"),
-      new SimpleDateFormat("yyyyMMdd") };
+  private static final String START = "^";
+  private static final String END = "$";
+  private static final String MM = "((0[1-9])|(1[0-2]))";
+  private static final String DD = "((0[1-9])|([12][0-9])|(3[01]))";
+  private static final String HH = "\\d{2}";
+  private static final String mm = "\\d{2}";
+  private static final String ss = "\\d{2}";
+  private static final String YYYY = "\\d{4}";
+  
+  private static Map<Pattern, SimpleDateFormat> formats = new LinkedHashMap<Pattern, SimpleDateFormat>()
+  {
+    {
+      // 8 digits assumed to be yyyyMMdd
+      put(Pattern.compile(START + YYYY + MM + DD + END), new SimpleDateFormat(
+          "yyyyMMdd"));
+      
+      // dd.MM.yyyy variations
+      put(Pattern.compile(START + DD + "-" + MM + "-" + YYYY + END),
+          new SimpleDateFormat("dd-MM-yyyy"));
+      put(Pattern.compile(START + DD + "/" + MM + "/" + YYYY + END),
+          new SimpleDateFormat("dd/MM/yyyy"));
+      
+      // MM.dd.yyyy variations
+      put(Pattern.compile(START + MM + "-" + DD + "-" + YYYY + END),
+          new SimpleDateFormat("MM-dd-yyyy"));
+      put(Pattern.compile(START + MM + "/" + DD + "/" + YYYY + END),
+          new SimpleDateFormat("MM/dd/yyyy"));
+      
+      // yyyy.MM.dd variations
+      put(Pattern.compile(START + YYYY + "-" + MM + "-" + DD + END),
+          new SimpleDateFormat("yyyy-MM-dd"));
+      put(Pattern.compile(START + YYYY + "/" + MM + "/" + DD + END),
+          new SimpleDateFormat("yyyy/MM/dd"));
+      
+      // yyyy.dd.MM variations
+      put(Pattern.compile(START + YYYY + "-" + DD + "-" + MM + END),
+          new SimpleDateFormat("yyyy-dd-MM"));
+      put(Pattern.compile(START + YYYY + "/" + DD + "/" + MM + END),
+          new SimpleDateFormat("yyyy/dd/MM"));
+      
+      // MM.yyyy variations
+      put(Pattern.compile(START + MM + "-" + YYYY + END), new SimpleDateFormat(
+          "MM-yyyy"));
+      put(Pattern.compile(START + MM + "/" + YYYY + END), new SimpleDateFormat(
+          "MM/yyyy"));
+      put(Pattern.compile(START + MM + "\\s" + YYYY + END),
+          new SimpleDateFormat("MM yyyy"));
+      
+      // yyyy.MM variations
+      put(Pattern.compile(START + YYYY + "-" + MM + END), new SimpleDateFormat(
+          "yyyy-MM"));
+      put(Pattern.compile(START + YYYY + "/" + MM + END), new SimpleDateFormat(
+          "yyyy/MM"));
+      put(Pattern.compile(START + YYYY + "\\s" + MM + END),
+          new SimpleDateFormat("yyyy MM"));
+      
+      put(Pattern.compile(START + YYYY + "-" + MM + "-" + DD + "T" + HH + ":"
+          + mm + ":" + ss + "Z" + END), new SimpleDateFormat(
+          "yyyy-MM-dd'T'HH:mm:ss'Z'"));
+      put(Pattern.compile(START + YYYY + "/" + MM + "/" + DD + "T" + HH + ":"
+          + mm + ":" + ss + "Z" + END), new SimpleDateFormat(
+          "yyyy/MM/dd'T'HH:mm:ss'Z'"));
+      
+      put(Pattern.compile(START + YYYY + "-" + MM + "-" + DD + "T" + HH + ":"
+          + mm + ":" + ss + END), new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss"));
+      put(Pattern.compile(START + YYYY + "/" + MM + "/" + DD + "T" + HH + ":"
+          + mm + ":" + ss + END), new SimpleDateFormat("yyyy/MM/dd'T'HH:mm:ss"));
+      
+      put(Pattern.compile(START + YYYY + "-" + MM + "-" + DD + "T" + HH + ":"
+          + mm + END), new SimpleDateFormat("yyyy-MM-dd'T'HH:mm"));
+      put(Pattern.compile(START + YYYY + "/" + MM + "/" + DD + "T" + HH + ":"
+          + mm + END), new SimpleDateFormat("yyyy/MM/dd'T'HH:mm"));
+      
+      put(Pattern.compile(START + YYYY + "-" + MM + "-" + DD + "\\s" + HH + ":"
+          + mm + END), new SimpleDateFormat("yyyy-MM-dd HH:mm"));
+      put(Pattern.compile(START + YYYY + "/" + MM + "/" + DD + "\\s" + HH + ":"
+          + mm + END), new SimpleDateFormat("yyyy/MM/dd HH:mm"));
+      
+      // put(Pattern.compile("^\\d{1,2}\\s[a-z]{4,}\\s\\d{4}$"),
+      // new SimpleDateFormat("dd MMMM yyyy"));
+      // put(Pattern.compile("^\\d{12}$"), new
+      // SimpleDateFormat("yyyyMMddHHmm"));
+      // put(Pattern.compile("^\\d{8}\\s\\d{4}$"), new SimpleDateFormat(
+      // "yyyyMMdd HHmm"));
+      // put(Pattern.compile("^\\d{1,2}-\\d{1,2}-\\d{4}\\s\\d{1,2}:\\d{2}$"),
+      // new SimpleDateFormat("dd-MM-yyyy HH:mm"));
+      // put(Pattern.compile("^\\d{4}-\\d{1,2}-\\d{1,2}\\s\\d{1,2}:\\d{2}$"),
+      // new SimpleDateFormat("yyyy-MM-dd HH:mm"));
+      // put(Pattern.compile("^\\d{1,2}/\\d{1,2}/\\d{4}\\s\\d{1,2}:\\d{2}$"),
+      // new SimpleDateFormat("MM/dd/yyyy HH:mm"));
+      // put(Pattern.compile("^\\d{4}/\\d{1,2}/\\d{1,2}\\s\\d{1,2}:\\d{2}$"),
+      // new SimpleDateFormat("yyyy/MM/dd HH:mm"));
+      // put(Pattern.compile("^\\d{1,2}\\s[a-z]{3}\\s\\d{4}\\s\\d{1,2}:\\d{2}$"),
+      // new SimpleDateFormat("dd MMM yyyy HH:mm"));
+      // put(Pattern.compile("^\\d{1,2}\\s[a-z]{4,}\\s\\d{4}\\s\\d{1,2}:\\d{2}$"),
+      // new SimpleDateFormat("dd MMMM yyyy HH:mm"));
+      // put(Pattern.compile("^\\d{14}$"), new
+      // SimpleDateFormat("yyyyMMddHHmmss"));
+      // put(Pattern.compile("^\\d{8}\\s\\d{6}$"), new SimpleDateFormat(
+      // "yyyyMMdd HHmmss"));
+      // put(Pattern
+      // .compile("^\\d{1,2}-\\d{1,2}-\\d{4}\\s\\d{1,2}:\\d{2}:\\d{2}$"),
+      // new SimpleDateFormat("dd-MM-yyyy HH:mm:ss"));
+      // put(Pattern
+      // .compile("^\\d{4}-\\d{1,2}-\\d{1,2}\\s\\d{1,2}:\\d{2}:\\d{2}$"),
+      // new SimpleDateFormat("yyyy-MM-dd HH:mm:ss"));
+      // put(Pattern
+      // .compile("^\\d{1,2}/\\d{1,2}/\\d{4}\\s\\d{1,2}:\\d{2}:\\d{2}$"),
+      // new SimpleDateFormat("MM/dd/yyyy HH:mm:ss"));
+      // put(Pattern
+      // .compile("^\\d{4}/\\d{1,2}/\\d{1,2}\\s\\d{1,2}:\\d{2}:\\d{2}$"),
+      // new SimpleDateFormat("yyyy/MM/dd HH:mm:ss"));
+      // put(Pattern
+      // .compile("^\\d{1,2}\\s[a-z]{3}\\s\\d{4}\\s\\d{1,2}:\\d{2}:\\d{2}$"),
+      // new SimpleDateFormat("dd MMM yyyy HH:mm:ss"));
+      // put(Pattern
+      // .compile("^\\d{1,2}\\s[a-z]{4,}\\s\\d{4}\\s\\d{1,2}:\\d{2}:\\d{2}$"),
+      // new SimpleDateFormat("dd MMMM yyyy HH:mm:ss"));
+      
+    }
+  };
+  
+  public static void addFormat(String pattern, String format)
+  {
+    formats.put(Pattern.compile(pattern), new SimpleDateFormat(format));
+  }
+  
+  public static void setFormats(Map<Pattern, SimpleDateFormat> newFormats)
+  {
+    formats.clear();
+    formats = newFormats;
+  }
   
   public static Date createDate(String dateStr)
   {
-    Date returnDate;
-    for (int i = 0; i < df.length; i++)
+    for (Pattern pattern : formats.keySet())
     {
       try
       {
-        returnDate = df[i].parse(dateStr);
-        return returnDate;
+        Matcher m = pattern.matcher(dateStr);
+        if (m.matches())
+        {
+          return formats.get(pattern).parse(dateStr);
+        }
       }
       catch(Exception ex)
       {
@@ -62,13 +183,12 @@ public class DateUtil
   {
     // Linked hashmap is important to preserve the order in which we try the
     // patterns, from most specific to least.
-    Map<SimpleDateFormat, Boolean> possibleFormats = new LinkedHashMap<SimpleDateFormat, Boolean>();
-    Date tmpDate;
+    Map<Pattern, Boolean> possibleFormats = new LinkedHashMap<Pattern, Boolean>();
     boolean allEmpty = true;
     
-    for (SimpleDateFormat fmt : df)
+    for (Pattern pattern : formats.keySet())
     {
-      possibleFormats.put(fmt, true);
+      possibleFormats.put(pattern, true);
     }
     
     for (String str : dateStr)
@@ -76,19 +196,34 @@ public class DateUtil
       if (str != null && str.length() > 0)
       {
         allEmpty = false;
-        for (SimpleDateFormat fmt : possibleFormats.keySet())
+        for (Pattern pattern : formats.keySet())
         {
-          if (possibleFormats.get(fmt))
+          if (possibleFormats.get(pattern))
           {
             try
             {
-              tmpDate = fmt.parse(str);
+              Matcher m = pattern.matcher(str);
+              // If it matches, try parsing the date with the associated
+              // formatter.
+              // Exceptions result in this formatter being dropped from
+              // consideration.
+              if (m.matches())
+              {
+                formats.get(pattern).parse(str);
+              }
+              // Violation, can't be this type.
+              else
+              {
+                // System.out.println("Regex: '" + pattern + "' failed on: '"
+                // + str + "'");
+                possibleFormats.put(pattern, false);
+              }
             }
             catch(Exception ex)
             {
-              System.out.println("Pattern: '" + fmt.toPattern()
-                  + "' failed on: '" + str + "'");
-              possibleFormats.put(fmt, false);
+              // System.out.println("Pattern: '" + pattern + "' failed on: '"
+              // + str + "'");
+              possibleFormats.put(pattern, false);
             }
           }
         }
@@ -97,11 +232,11 @@ public class DateUtil
     
     if (!allEmpty)
     {
-      for (SimpleDateFormat fmt : possibleFormats.keySet())
+      for (Pattern pattern : possibleFormats.keySet())
       {
-        if (possibleFormats.get(fmt))
+        if (possibleFormats.get(pattern))
         {
-          return fmt;
+          return formats.get(pattern);
         }
       }
     }
@@ -109,28 +244,96 @@ public class DateUtil
     return null;
   }
   
-  public static void main(String args[])
+  public static SimpleDateFormat guessFormat(String dateStr[])
   {
-    List<String> dateStrings = new ArrayList<String>();
-    
-    dateStrings.add("12/14/2015 21:14");
-    // dateStrings.add("21/12/2015");
-    // dateStrings.add("");
-    // dateStrings.add(null);
-    // dateStrings.add("22/2/2015");
-    
-    SimpleDateFormat fmt = DateUtil.guessFormat(dateStrings);
-    System.out.println("Format: '" + fmt.toPattern() + "'");
-    for (String dateStr : dateStrings)
+    List<String> dateList = new ArrayList<String>();
+    for (String str : dateStr)
+    {
+      dateList.add(str);
+    }
+    return guessFormat(dateList);
+  }
+  
+  public static void formatList(SimpleDateFormat fmt, String strings[])
+  {
+    for (String str : strings)
     {
       try
       {
-        System.out.println(new Timestamp(fmt.parse(dateStr).getTime()));
+        System.out.println(new Timestamp(fmt.parse(str).getTime()));
       }
       catch(Exception ex)
       {
         // ex.printStackTrace();
       }
     }
+  }
+  
+  public static void test(String strings[])
+  {
+    SimpleDateFormat fmt = DateUtil.guessFormat(strings);
+    System.out.println("Format: [" + StringUtils.join(strings, ",") + "] = '"
+        + ((fmt != null) ? fmt.toPattern() : "None") + "'");
+    formatList(fmt, strings);
+  }
+  
+  public static void main(String args[])
+  {
+    // Basic ISO Date
+    // test(new String[] { "19991231" });
+    
+    // Test 1: MM-dd-yyyy
+    // test(new String[] { "12-21-2016" });
+    // test(new String[] { "12/21/2016" });
+    
+    // Test 2: dd-MM-yyyy
+    // test(new String[] { "21-12-2016" });
+    // test(new String[] { "21/12/2016" });
+    
+    // Test 3: yyyy.MM.dd variations
+    // test(new String[] { "2015-12-21" });
+    // test(new String[] { "2015/12/21" });
+    
+    // Test 4: yyyy.dd.MM variations
+    // test(new String[] { "2015-21-12" });
+    // test(new String[] { "2015-21-12" });
+    
+    // Test 5: MM.yyyy
+    // test(new String[] { "12/2015" });
+    // test(new String[] { "12-2015" });
+    // test(new String[] { "12 2015" });
+    
+    // Test 6: MM.yyyy
+    // test(new String[] { "2015-12" });
+    // test(new String[] { "2015/12" });
+    // test(new String[] { "2015 12" });
+    
+    // Test 7: yyyy.MM.dd'T'HH:mm:ss'Z' variations
+    // ISO_INSTANT
+    //test(new String[] { "1968-07-29T08:30:12Z" });
+    //test(new String[] { "1968/07/29T08:30:12Z" });
+    
+    // Test 8: yyyy.MM.dd'T'HH:mm:ss variations
+    // test(new String[] { "1968-07-29T08:30:12" });
+    // test(new String[] { "1968/07/29T08:30:12" });
+    
+    // Test 9: yyyy.MM.dd'T'HH:mm variations
+    // test(new String[] { "1968-07-29T08:30" });
+    // test(new String[] { "1968/07/29T08:30" });
+    
+    // Test 10: yyyy.MM.dd HH:mm variations
+    // test(new String[] { "1968-07-29 08:30" });
+    // test(new String[] { "1968/07/29 08:30" });
+    
+    // Test 4: MM/dd/yyyy hh:ss
+    // test(new String[] { "12/21/2015 21:14" });
+    
+    // dateStrings.add("21-12-2015");
+    // dateStrings.add("21/12/2015");
+    //
+    // dateStrings.add("");
+    // dateStrings.add(null);
+    // dateStrings.add("22/2/2015");
+    
   }
 }
