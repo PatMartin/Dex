369d3287cbd836e54ffdc0055ef6f7d018a146b5
PatMartin <patrick145@msn.com>
2018-03-13 11:26:28 -0400

    Added readToString routine

--- src/com/dexvis/util/StreamUtil.java
+++ src/com/dexvis/util/StreamUtil.java
@@ -1,100 +1,113 @@
 package com.dexvis.util;
 
-import java.io.*;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 
 public class StreamUtil
 {
   public static void nbTransfer(InputStream is, OutputStream os, String label)
-    throws IOException
+      throws IOException
   {
     int nread;
     byte buf[] = new byte[8192];
-
+    
     while ((is.available() > 0) && ((nread = is.read(buf)) != -1))
     {
       if (label.length() > 0)
       {
-	System.out.println(label + new String(buf, 0, nread));
+        System.out.println(label + new String(buf, 0, nread));
       }
       os.write(buf, 0, nread);
     }
   }
-
+  
   public static void nbTransfer(InputStream is, OutputStream os)
-    throws IOException
+      throws IOException
   {
     nbTransfer(is, os, "");
   }
-
+  
   public static void transfer(InputStream is, OutputStream os, String label)
-    throws IOException
+      throws IOException
   {
     int nread;
     byte buf[] = new byte[8192];
-
+    
     while ((nread = is.read(buf)) != -1)
     {
       if (label.length() > 0)
       {
-	System.out.println(label + new String(buf, 0, nread));
+        System.out.println(label + new String(buf, 0, nread));
       }
       os.write(buf, 0, nread);
     }
   }
-
+  
   public static void transfer(InputStream is, OutputStream os)
-    throws IOException
+      throws IOException
   {
     transfer(is, os, "");
   }
-
+  
   public static void nbTransfer(InputStream is, OutputStream os, String label,
-		    int numTries, long sleepInterval)
-    throws IOException
+      int numTries, long sleepInterval) throws IOException
   {
     int nread;
     byte buf[] = new byte[8192];
-
-    while (available(is, numTries, sleepInterval) &&
-	   ((nread = is.read(buf)) != -1))
+    
+    while (available(is, numTries, sleepInterval)
+        && ((nread = is.read(buf)) != -1))
     {
       if (label.length() > 0)
       {
-	System.out.println(label + new String(buf, 0, nread));
+        System.out.println(label + new String(buf, 0, nread));
       }
       os.write(buf, 0, nread);
     }
   }
-
+  
   public static void nbTransfer(InputStream is, OutputStream os, int numTries,
-		    long sleepInterval)
-    throws IOException
+      long sleepInterval) throws IOException
   {
     nbTransfer(is, os, "", numTries, sleepInterval);
   }
-
-  public static boolean available(
-    InputStream is, int numTries, long sleepInterval)
+  
+  public static boolean available(InputStream is, int numTries,
+      long sleepInterval)
   {
     try
     {
-      for (int i=0; i<numTries; i++)
+      for (int i = 0; i < numTries; i++)
       {
-	if (is.available() > 0)
-	  return true;
-	
-	try { Thread.sleep(sleepInterval); } catch (Exception ex) {}
+        if (is.available() > 0)
+          return true;
+        
+        try
+        {
+          Thread.sleep(sleepInterval);
+        }
+        catch(Exception ex)
+        {
+        }
       }
     }
-    catch (Exception ex)
+    catch(Exception ex)
     {
     }
-
+    
     return false;
   }
-
-  public static String readFile(File file)
-    throws IOException
+  
+  public static String readToString(InputStream is)
+  {
+    java.util.Scanner s = new java.util.Scanner(is).useDelimiter("\\A");
+    return s.hasNext() ? s.next() : "";
+  }
+  
+  public static String readFile(File file) throws IOException
   {
     int nread;
     byte buf[] = new byte[8192];
