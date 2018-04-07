0c64c54b3b7134192a0cd587a217e5d842e7c7aa
PatMartin <patrick145@msn.com>
2018-04-07 04:42:22 -0400

    Removed deprecated import

--- src/com/dexvis/db/JdbcTable.java
+++ src/com/dexvis/db/JdbcTable.java
@@ -6,7 +6,6 @@ import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.text.DateFormat;
-import java.text.ParseException;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
